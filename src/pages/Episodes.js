2eeef57caff1b6a94314c5ade6a2d2f0d182f876
fabiola29298 <fabiola29298@gmail.com>
2021-06-02 11:12:28 -0400

    styles de episodes

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -3,14 +3,19 @@ import getEpisode from '../utils/getEpisode';
   const Episodes = async () => {
     const episodes = await getEpisode();
     const view = `
-    <div class="Episodes">
+    <div class="episode-card">
       ${episodes.map(episode => `
-        <article class="episode-item">
-          <a href="#/episodes/${episode.episode_id}/">
-            <h2> ${episode.title} </h2>
-            <h2> ${episode.season} </h2>
-          </a>
-        </article>
+      <a href="#/episodes/${episode.episode_id}/">
+        <div class="episode-item">
+            <div class="column">
+              <h5> ${episode.title} </h5>
+              <p> Season: ${episode.season} </p>
+            </div>
+            <div class="column2">
+            <p> More> </p>
+            </div>
+        </div>
+      </a>
       `).join('')}
     </div>
   `;
