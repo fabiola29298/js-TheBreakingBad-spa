7a45541cb204a707d52f68e768a8bbf8287253b1
fabiola29298 <fabiola29298@gmail.com>
2021-06-02 10:03:53 -0400

    css de header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,10 +1,10 @@
 const Header = () => {
   const view = `
-    <div class="Header-main">
-      <div class="Header-logo">
+    <div class="header-main">
+      <div class="header-logo">
         <h1> <a href="/"> The Breaking Bad Web</a>
       </div>
-      <div class="Header-nav">
+      <div class="header-nav">
        <a href="#/characters/"> Characters</a>
        <a href="#/episodes/"> Episodes</a>
        </div>
